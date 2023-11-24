94e051e37484d4c46bf770d00d0f14206affe6ea
Tyler Michael <tylermichael@users.noreply.github.com>
2023-11-24 00:25:06 -0500

    add guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,26 @@
+use std::cmp::Ordering;
+use std::io::stdin;
+
+use rand::Rng;
+
+fn main()-> Result<(), ()>{
+    let num: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The random number is: {num}");
+    println!("Enter a number from 1-100: ");
+
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess).expect("failed to read stdin");
+        let guess: i32 = guess.trim().parse().expect("You must enter a number!");
+
+        match num.cmp(&guess) {
+            Ordering::Equal => {
+                println!("You guessed correctly!");
+                return Ok(())
+            },
+            Ordering::Greater => println!("The number is greater"),
+            Ordering::Less => println!("The number is lower")
+        }
+    }
+}
